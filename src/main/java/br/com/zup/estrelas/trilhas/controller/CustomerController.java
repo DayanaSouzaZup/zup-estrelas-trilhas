df79d707d2e3d43bf27ff2fdc0baa204a74e3aa9
DayanaSouzaZup <dayana.souza@zup.com.br>
2021-01-28 09:57:42 -0300

    Alteração Método UpdateCustomer

--- src/main/java/br/com/zup/estrelas/trilhas/controller/CustomerController.java
+++ src/main/java/br/com/zup/estrelas/trilhas/controller/CustomerController.java
@@ -55,7 +55,7 @@ public class CustomerController {
 	}
 
 	@PutMapping(path = "/{cpf}", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public MessageDTO updateCliente(@PathVariable String cpf, @RequestBody UpdateCustomerDTO customer) {
+	public MessageDTO updateCustomer(@PathVariable String cpf, @RequestBody UpdateCustomerDTO customer) {
 
 		return customerService.updateCustomer(cpf, customer);
 	}
